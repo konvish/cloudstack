3bc0cc78888c0f0fd2604978432743bd92801f29
kong <konvish@163.com>
2016-07-27 14:36:00 +0800

    增加cs-spring-zkconfig的注释

--- cs-spring-zkconfig/src/main/java/com/kong/cloudstack/sz/util/EncryptUtil.java
+++ cs-spring-zkconfig/src/main/java/com/kong/cloudstack/sz/util/EncryptUtil.java
@@ -2,13 +2,14 @@ package com.kong.cloudstack.sz.util;
 
 import com.kong.cloudstack.utils.AESUtil;
 /**
+ * 加密工具
  * Created by kong on 2016/1/24.
  */
 public class EncryptUtil {
     private static final byte[] encryptTag = new byte[]{(byte)83, (byte)1};
     private static final char oneChar = '\u0001';
     public static final String encryptStr = "S\u0001";
-    public static final String encryptKey = "i love thinkoy, and i love change";
+    public static final String encryptKey = "i love change";
 
     public EncryptUtil() {
     }
@@ -18,11 +19,11 @@ public class EncryptUtil {
     }
 
     public static String encrypt(String cryptStr) throws Exception {
-        return "S\u0001" + AESUtil.aesEncrypt(cryptStr, "i love thinkoy, and i love change");
+        return "S\u0001" + AESUtil.aesEncrypt(cryptStr, "i love change");
     }
 
     public static String decrypt(String encryptStr) throws Exception {
         String realEncryptStr = encryptStr.substring(2);
-        return AESUtil.aesDecrypt(realEncryptStr, "i love thinkoy, and i love change");
+        return AESUtil.aesDecrypt(realEncryptStr, "i love change");
     }
 }
